7e19a10c3b1798bb71c995da4827d58e9b7fe376
Sovann <sovann@youdingapp.com>
2023-10-04 23:55:35 +0700

    update

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/chhumsovann/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/chhumsovann/Documents/vann-dev/flutter-dev/art_button_kh"
+export "FLUTTER_APPLICATION_PATH=/Users/chhumsovann/Documents/vann-dev/flutter-dev/animation_kh"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
